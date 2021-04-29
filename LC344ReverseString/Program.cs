452356c2770a96bfd4f272e528dfdc67ac992fe2
Carlos Alvarado <wikiwakawakawee@gmail.com>
2021-04-29 10:02:30 -0500

    Add project files.

---
+++ LC344ReverseString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LC344ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] test = { 'h', 'e', 'l', 'l', 'o' };
+            ReverseString(test);
+            foreach(char letter in test)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+
+        public static char[] ReverseString(char[] c)
+        {
+            char front;
+            char back;
+            for (int i = 0, j = c.Length - 1; i < c.Length / 2; i++, j--)
+            {
+                front = c[i];
+                back = c[j];
+
+                c[i] = back;
+                c[j] = front;
+            }
+            return c;
+        }
+    }
+}
